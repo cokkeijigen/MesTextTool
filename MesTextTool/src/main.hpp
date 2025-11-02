f37c2fff9ee8e18b9b4adb8de8a684db200321a5
cokkeijigen <itsukezigen@gmail.com>
2025-11-02 20:44:09 +0800

    Update main.hpp

--- MesTextTool/src/main.hpp
+++ MesTextTool/src/main.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <console.hpp>
 
 inline constexpr char _tool_version_name_[] = "1.3.3";
 inline constexpr char _tool_name_[] = "MesTextTool";
+console::helper xcsl::helper{ _tool_name_ };
