9ce6b3ce541c440e3183add87c10de51bf74630c
cokkeijigen <itsukezigen@gmail.com>
2025-11-02 20:44:08 +0800

    Update main.cpp

--- MesTextTool/src/main.cpp
+++ MesTextTool/src/main.cpp
@@ -30,13 +30,13 @@ static auto run(int argc, const char** args) -> void
 			enable_log = { "-log" == arg || "-LOG" == arg };
 		}
 
-		auto input_info = static_cast<const mes::script_info*>(nullptr);
-		auto input_cdpg = static_cast<uint32_t>(mes::multi_script_helper::defualt_code_page);
-		auto input_path = std::string_view{ argc == 3 ? args[2] : args[1] };
-		auto self_path = std::string_view{ args[0] };
-		auto work_path = std::string_view{ ".\\" };
+		auto input_info{ static_cast<const mes::script_info*>(nullptr) };
+		auto input_cdpg{ static_cast<uint32_t>(mes::multi_script_helper::defualt_code_page) };
+		auto input_path{ std::string_view{ argc == 3 ? args[2] : args[1] } };
+		auto self_path { std::string_view{ args[0] }};
+		auto work_path { std::string_view{ ".\\" }  };
 
-		size_t find_pos = self_path.find_last_of("\\/");
+		size_t find_pos{ self_path.find_last_of("\\/") };
 		if (find_pos != std::string_view::npos)
 		{
 			work_path = self_path.substr(0, find_pos + 1);
@@ -66,7 +66,7 @@ static auto run(int argc, const char** args) -> void
 
 		if (!enable_log)
 		{
-			console::writeline("[PROCESSING]...");
+			xcsl::helper.writeline("[PROCESSING]...\n");
 		}
 
 		mes::multi_script_helper helper{ input_path, work_path, input_info, input_cdpg };
@@ -74,49 +74,45 @@ static auto run(int argc, const char** args) -> void
 			// on success callback
 			[&](std::string_view ipt, std::string_view opt) -> void
 			{
-				if (enable_log) {
-					console::fmt::write("[SUCCESSFULLY] \n%s\n\n", opt.data());
+				if (enable_log) 
+				{
+					xcsl::helper.write("[SUCCESSFULLY] \n%s\n\n", opt.data());
 				}
 			},
 			// on failure callback
 			[&](std::string_view ipt) -> void
 			{
-				if (enable_log) {
-					console::fmt::write<console::cdpg::dDfault, console::txt::dark_red>(
-						"[ERROR: %s]\n%s\n\n", helper.get_err_msg().data(), ipt.data()
-					);
+				if (enable_log) 
+				{
+					xcsl::helper.set_attrs(console::attrs::text_dark_red);
+					xcsl::helper.write("[ERROR: %s]\n%s\n\n", helper.get_err_msg().data(), ipt.data());
+					xcsl::helper.reset_attrs();
 				}
 			}
 		);
 
 		if (!enable_log)
 		{
-			console::clear();
-			console::writeline("[COMPLETE]\n");
+			xcsl::helper.clear();
+			xcsl::helper.writeline("[COMPLETE]\n");
 		}
 	}
 	catch (const std::exception& err)
 	{
-		console::fmt::write<console::cdpg::dDfault, console::txt::dark_red>(
-			"%s\n", err.what()
-		);
+		xcsl::helper.set_attrs(console::attrs::text_dark_red);
+		xcsl::helper.write("%s\n", err.what());
+		xcsl::helper.reset_attrs();
 	}
 }
 
-
 auto main(const int argc, const char** args) -> int 
 {
-	
-	console::make(_tool_name_, false);
-	
 	auto time { utils::time_counter<double>([&]() { run(argc, args); }) };
-	console::fmt::write<console::cdpg::dDfault, console::txt::dark_yellow>
-	(
-		"MesTextTool: Ver %s.\n"
-		"GitHub: https://github.com/cokkeijigen/MesTextTool\n"
-		"Time: %llf\n", _tool_version_name_, time
-	);
-
-	console::pause();
+	xcsl::helper.set_attrs(console::attrs::text_dark_yellow);
+	xcsl::helper.write("MesTextTool: Ver %s.\n", _tool_version_name_);
+	xcsl::helper.write("GitHub: https://github.com/cokkeijigen/MesTextTool\n");
+	xcsl::helper.write("Time: %llf\n", time);
+	xcsl::helper.reset_attrs();
+	xcsl::helper.read_anykey();
 	return { 0x114514 };
 }
